96ab52299318a34f1351aa45557e42c6d49189d7
Hirdhya Khanna <108194883+Hirdhyakk@users.noreply.github.com>
2025-12-12 18:39:51 +0530

    Update Program-1.java

--- Program-1.java
+++ Program-1.java
@@ -1,37 +1,68 @@
+
+import java.util.Scanner;
+
+class Calculator {
+
+    public double calculate(double a, double b, String operation) {
+        switch (operation) {
+            case "+":
+                return add(a, b);
+            case "-":
+                return subtract(a, b);
+            case "*":
+                return multiply(a, b);
+            case "/":
+                return divide(a, b);
+            default:
+                throw new IllegalArgumentException("Invalid operation type");
+        }
+
+    }
+
+    private double add(double a, double b) {
+        return a + b;
+    }
+
+    private double subtract(double a, double b) {
+        return a - b;
+    }
+
+    private double multiply(double a, double b) {
+        return a * b;
+    }
+
+    private double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         Calculator calc = new Calculator();
 
         System.out.print("Enter value for a: ");
-
         double a = sc.nextDouble();
 
         System.out.print("Enter value for b: ");
-
         double b = sc.nextDouble();
 
         System.out.print("Enter operation (+  -  *  /): ");
-
         String operation = sc.next();
 
         try {
-
             double result = calc.calculate(a, b, operation);
-
             System.out.println("Result: " + result);
-
         } catch (Exception e) {
-
             System.out.println("Error: " + e.getMessage());
-
         }
 
         sc.close();
-
     }
 
 }

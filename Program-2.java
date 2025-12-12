02f6301c61879a0af1a74fdc003dbfd7a9616191
Hirdhya Khanna <108194883+Hirdhyakk@users.noreply.github.com>
2025-12-12 18:40:33 +0530

    Update Program-2.java

--- Program-2.java
+++ Program-2.java
@@ -1,11 +1,9 @@
 public class OddSeries {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter value of a: ");
-
         int a = sc.nextInt();
 
         int number = 1;
@@ -13,21 +11,14 @@ public class OddSeries {
         System.out.print("Output: ");
 
         for (int i = 1; i <= a; i++) {
-
             System.out.print(number);
-
             number += 2;
-
             if (i < a) {
-
                 System.out.print(", ");
-
             }
-
         }
 
         sc.close();
-
     }
 
 }
